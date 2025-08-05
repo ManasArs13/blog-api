7f3ccc2f6ef6af6299ed1c6702d2c6329b605fdf
ManasArs13 <manasyan.a.s@ya.ru>
2025-08-05 08:31:33 +0300

    БД: создана основная структура
    - Созданы миграции;
    - Добавлены модели;
    - Добавлены фабрики и сиды;
    - Добавлены отношения:
      * User hasMany Post, Comment;
      * Post belongsTo User, hasMany Comment;
      * Comment belongsTo User, Post

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
@@ -19,30 +18,15 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
-        'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function posts(): HasMany
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,47 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    // Конфигурация количества записей
+    public const USER_COUNT = 10;
+    public const POST_COUNT = 5;
+    public const COMMENT_COUNT = 6;
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(Self::USER_COUNT)
+            ->has(
+                Post::factory(Self::POST_COUNT)
+                    ->hasComments(SELF::COMMENT_COUNT)
+            )
+            ->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->printStatistics();
+    }
+
+    /**
+     * Вывод статистики по заполненным данным
+     */
+    protected function printStatistics()
+    {
+        $this->command->info('Database seeded successfully!');
+        $this->command->table(
+            ['Таблица', 'Всего записей', 'Выполнено сейчас'],
+            [
+                ['Пользователи', User::count(), self::USER_COUNT],
+                ['Посты', Post::count(), self::POST_COUNT],
+                ['Комментарии', Comment::count(), self::COMMENT_COUNT],
+            ]
+        );
     }
 }
